9cc6c0791f027b74cd6cfc4a8c227181f86a105f
RAJA ABDUL-REHMAN <107124707+Raja786jee@users.noreply.github.com>
2023-07-05 12:23:54 +0500

    Create intro.js
    just an introduction of my name by using some js commands.

---
+++ intro.js
@@ -0,0 +1,5 @@
+function age(){
+  var myage=prompt("write down your age here:");
+  return myage;
+}
+console.log("HY! Uzair bahi myself Abdul-Rehman here, and my age is " + age() +". Thank u for checking my portfolio!");
